083d8716780d2ecd3895dee81766be9d8cf71540
carlosrs14 <crincones578@gmail.com>
2025-11-17 21:40:52 -0500

    vuln level 2 done

--- app/apis/auth/services/update_profile_service.py
+++ app/apis/auth/services/update_profile_service.py
@@ -3,7 +3,7 @@ from typing import Union
 from apis.auth.utils import get_current_user, get_user_by_username
 from db.models import User
 from db.session import get_db
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from typing_extensions import Annotated
@@ -24,6 +24,12 @@ def update_profile(
     current_user: Annotated[User, Depends(get_current_user)],
     db: Session = Depends(get_db),
 ):
+    if current_user.username != user.username:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not enough permissions",
+        )
+
     db_user = get_user_by_username(db, user.username)
 
     for var, value in user.dict().items():
